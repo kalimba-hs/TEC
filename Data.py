fe6ecf4a5ccddf8c6fed41723b471755ea4e1721
Andrey <andreyherrera14.ah@gmail.com>
2019-04-10 14:19:53 -0600

    Main

---
+++ Data.py
@@ -0,0 +1,13 @@
+from Person import *
+from Airplane import *
+userList = []
+planesList = []
+
+p1 = User("moi", "vindas", 22, "moi@hotmail.com", "2016", "123", "admin")
+p2 = User("Kali", "Herrera", 19, "kali@gmail.com", "2019", "123", "guest")
+userList.append(p1)
+userList.append(p2)
+
+
+ap1 = ("ll3", "2015", "611", "Emirates", 200, "available")
+planesList.append(ap1)
